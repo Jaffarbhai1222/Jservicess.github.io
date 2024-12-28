4c1724b182cdfe9469bd60e73e1027ac04b12a6a
Jaffarbhai1222 <161254367+Jaffarbhai1222@users.noreply.github.com>
2024-12-28 12:11:22 -0800

    Delete javascript.js

--- javascript.js
+++
@@ -1,5 +0,0 @@
-// Scroll to Services Section
-document.querySelector('.cta-btn').addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector('#services').scrollIntoView({ behavior: 'smooth' });
-});
